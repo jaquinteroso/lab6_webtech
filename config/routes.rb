03748955e25b578c557abd7124c728140971484c
jaquinteroso <jaquinterosou@gmail.com>
2026-04-30 08:56:14 -0400

    CRUD routes changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  resources :owners, only: [:index, :show]
-  resources :pets, only: [:index, :show]
-  resources :vets, only: [:index, :show]
-  resources :appointments, only: [:index, :show]
-
   # Ruta principal
   root "owners#index"
+
+  resources :owners
+  resources :pets
+  resources :vets
+  resources :appointments do
+    resources :treatments, only: [:new, :create, :edit, :update, :destroy]
+  end
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
